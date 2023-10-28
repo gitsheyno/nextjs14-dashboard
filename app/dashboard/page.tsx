467d9c51cf41600599f98a35aa0f083f8f4762d2
shayan heidary <hdrydeveloper@gmail.com>
2023-10-28 16:01:32 +0200

    fixed for production

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -51,9 +51,8 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* @ts-expect-error server component */}
         <RevenueChart revenue={revenue} />
-        {/* @ts-expect-error server component */}
+
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
