b4d83e78aa97cf5cc8558dfb61e2e9486f830c8f
shayan heidary <hdrydeveloper@gmail.com>
2023-11-03 13:37:37 +0100

    added error page for invoioces

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { deleteInvocie } from "@/app/lib/actions";
+import { deleteInvoice } from "@/app/lib/actions";
 
 export function CreateInvoice() {
   return (
@@ -26,15 +26,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithID = deleteInvocie.bind({}, id);
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
-      <form action={deleteInvoiceWithID}>
-        <button className="rounded-md border p-2 hover:bg-gray-100">
-          <span className="sr-only">Delete</span>
-          <TrashIcon className="w-5" />
-        </button>
-      </form>
-    </>
+    <form action={deleteInvoiceWithId}>
+      <button className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-4" />
+      </button>
+    </form>
   );
 }
