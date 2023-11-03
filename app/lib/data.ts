7c801d434ee8d920c2f77cd0f899d9f7062df698
shayan heidary <hdrydeveloper@gmail.com>
2023-11-03 13:49:18 +0100

    not-found error handled

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -181,6 +181,7 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
+    throw new Error("failed to fetch invocie");
   }
 }
 
